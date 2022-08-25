84a7415cf40705debb724a7a7794e4f822b747a2
Richd0tcom <tuberich0@gmail.com>
2022-08-25 22:29:28 +0100

    airport filter

--- public/js/airports.js
+++ public/js/airports.js
@@ -1,48 +1,64 @@
-const suggestions = document.querySelector('.menu-items')
+
+
+const suggestion1 = document.querySelector('.menuItem1')
+const suggestion2 = document.querySelector('.menuItem2')
+suggestion1.addEventListener('click', calculate1)
+suggestion2.addEventListener('click', calculate)
 const airportForm = document.querySelectorAll('.airport-form')
-airportForm.forEach(item=> item.addEventListener('keyup',displayMatches));
-airportForm.forEach(item=> item.addEventListener('input',displayMatches))
+
 
 // let names = ["BTC","XRP","ETH","BCH","ADA","XEM","LTC","XLM","TRX","MIOTA","DASH","EOS","XMR","NEO","QTUM","BTG","ETC","ICX","LSK","XRB","OMG","SC","BCN","ZEC","XVG","BCC","DCN","BTS","PPT","DOGE","BNB","KCS","STRAT","ARDR","SNT","STEEM","USDT","WAVES","VEN","DGB","KMD","DRGN","HSR","KIN","ETN","GNT","REP","VERI","ETHOS"]
-   
+let pp = [];
+// let html;
+
    async function getAirports(){
     const res = await fetch(`http://localhost:5000/airports`)
     console.log(res)
     const data = await res.json();
     console.log(data)
-    return data.data
+    pp.push(...data.data)
+    console.log(pp)
    }
 
-const pp = getAirports()
+
+getAirports()
 
 
 function findMatches(wordToMatch, airports) {
     return airports.filter( port => {
     // here we need to figure out if the cat breed matches what was searched
     const regex = new RegExp(wordToMatch, 'gi');
-    return port.name.match(regex);
+    return port.iso_country.match(regex);
+
     
 });}
 
 
   function displayMatches() {
     console.log(this.value)
+    console.log(this.id)
   const matchArray = findMatches(this.value, pp);
+  console.log(matchArray)
   const html = matchArray.map(pt => {
 
-
-    // const regex = new RegExp(this.value, 'gi');
-    // const catName = ct.name.replace(regex, `<span class="bg-[#ffc600]">${this.value}</span>`);
     return `
-      <li class="dropdown-item port" data-uid="${pt.id}" data-value="${pt.name}">
+      <li class="dropdown-item port" data-uid="${pt.id}" data-long="${pt.longitude_deg} data-lat="${pt.latitude_deg}">
         ${pt.name}
       </li>
     `;
     
   }).join('');
-  suggestions.innerHTML = html;
- }
 
+  console.log(typeof html)
+  if(this.id == 'from'){
+    suggestion1.innerHTML = html
+    return
+  }else if(this.id =="to"){
+    suggestion2.innerHTML = html
+    return
+  }
+ }
+ 
 //  if (e.target.matches('.port')) {
         
 //     airport.value = e.target.dataset.value;
@@ -50,3 +66,14 @@ function findMatches(wordToMatch, airports) {
 //     console.log(e.target.dataset.uid)
 //     console.log(catid)
 //   }
+
+function calculate1(e){
+  let longitude = e.target.dataset.long 
+  let latitude = e.target.dataset.lat
+    
+}
+function calculate1(e){}
+//   e.target.datatset.long
+// }
+airportForm.forEach(item=> item.addEventListener('keyup',displayMatches));
+airportForm.forEach(item=> item.addEventListener('input',displayMatches));
