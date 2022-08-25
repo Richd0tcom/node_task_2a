8a4d1df8e0e6e5109030c03a8e70a606f8db510c
Richd0tcom <tuberich0@gmail.com>
2022-08-25 19:18:14 +0100

    Feat: weather api
    Implemented the weather api to get a current locations weather data

--- public/js/weather.js
+++ public/js/weather.js
@@ -1,1 +1,45 @@
-process.env.WEATHER_API_KEY
+
+
+const details = document.getElementById('weather-details')
+const weatherForm = document.getElementById('weather-form')
+
+
+
+async function displayWeather(e){
+    e.preventDefault()
+     const res = await fetch(`http://api.weatherapi.com/v1/current.json?key=3c6bb061f8d84eb3a5a134932222508&q=London`);
+     const data = await res.json()
+     console.log(data);
+
+     const html =   `
+     <table class="table table-hover">
+     <thead>
+       <tr>
+         <th scope="col">Location</th>
+         <th scope="col">${data.location.name}</th>
+       </tr>
+     </thead>
+     <tbody>
+       <tr class="">
+         <th scope="row">Degree</th>
+         <td>${data.current.temp_f}F</td>
+         
+       </tr>
+       <tr>
+         <th scope="row">Type of weather</th>
+         <td>${data.current.condition.text}</td>
+       </tr>
+       <tr>
+         <th scope="row">Humidity</th>
+         <td>${data.current.humidity}</td>
+       </tr>
+       
+     </tbody>
+   </table>
+     
+     `
+
+    details.innerHTML =html;
+    this.reset()
+}
+weatherForm.addEventListener('submit',displayWeather);
